1621f6e2de6ece6e05192a9dbddca01f34bada08
Eduardo Dobay <edudobay@gmail.com>
2016-07-10 19:51:13 -0300

    Bye bye legacy

--- imaging/core/__init__.py
+++ imaging/core/__init__.py
@@ -1,4 +1,3 @@
-from imaging.core.legacy import *
 from imaging.core.geometry import *
 from imaging.core.image import *
 from imaging.core.units import *
--- imaging/cut.py
+++ imaging/cut.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import os
 from imaging.core import *
-from imaging.core.image import get_channel_data
-from imaging.core.signal import find_bbox
+from imaging.core.page import center_on_page
+from imaging.core.image import get_channel_data, offset_image
+from imaging.core.signal import find_bbox, split_into_nonempty_areas
 
 from PIL import Image # type: ignore
 import numpy as np # type: ignore
@@ -10,10 +11,28 @@ from argparse import *
 
 np.set_printoptions(threshold=np.nan)
 
-def find_image_bbox(image):
-    blended = alpha_blend_over_background(image, 'white').convert('LA')
-    data = 1 - get_channel_data(blended, 'L') / 255
-    return find_bbox(image)
+def get_black_level(image):
+    return 1 - get_channel_data(image, 'L') / 255
+
+def get_opacity(image):
+    return get_channel_data(image, 'A') / 255
+
+def get_stripe(image, top, bottom):
+    return image.crop(0, top, image.width, bottom)
+
+def get_stripes(image):
+    image = image.convert('LA')
+    black = get_black_level(image)
+    opacity = get_opacity(image)
+
+    bbox_left, bbox_right = find_bbox(black, axis=0)
+
+    stripes = split_into_nonempty_areas(opacity, axis=1)
+
+    bbox_target_x, _ = center_on_page((bbox_right - bbox_left, 0), image.size)
+    new_image = offset_image(image, (bbox_target_x - bbox_left, 0))
+
+    return [get_stripe(new_image, top, bottom) for (top, bottom) in stripes]
 
 if __name__ == '__main__':
 
@@ -29,13 +48,7 @@ if __name__ == '__main__':
     for infile in args.infile:
         with Image.open(infile) as im:
             print(infile)
-            ip = ImageProcessor(im)
-            ip.find_bbox()
-            if args.page_width:
-                # TODO: Improve our interfaces so we don't set this directly
-                ip.width = int(args.page_width)
-
-            images = ip.extract_stripes(ip.get_stripes(), crop=args.crop)
+            images = get_stripes(im)
             
             basename = os.path.splitext(os.path.basename(infile))[0] + '_%02d.png'
             dirname = os.path.dirname(infile) if args.output_dir is None else args.output_dir
