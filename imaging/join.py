11e3e930ca4fcfc71385052befa34bbe4442a576
Eduardo Dobay <edudobay@gmail.com>
2016-07-10 22:01:49 -0300

    Use arguments instead of hard-coded defaults in join.py

--- imaging/join.py
+++ imaging/join.py
@@ -77,18 +77,22 @@ def main():
     parser.add_argument('-f', dest='format', default='collage_%02d.png', help='format for naming output files')
     parser.add_argument('-e', dest='fill_page_height', action='store_true', help='fill all available vertical space')
     parser.add_argument('-S', dest='max_spacing', type=Distance, help='maximum spacing between systems')
+    parser.add_argument('-d', '--dpi', type=float, dest='input_dpi', help='set DPI of input image')
+    parser.add_argument('-W', '--width', type=Distance, dest='page_width', help='set output page width')
+    parser.add_argument('-H', '--height', type=Distance, dest='page_height', help='set output page height')
     args = parser.parse_args()
 
-    DPI = 300
-    MM = DPI / 25.4
-    page_width  = round(210*MM)
-    page_height = round(297*MM)
+    dpi = args.input_dpi
+    MM = Distance(1, 'mm').get_logical(dpi)
+    page_width  = args.page_width.get_logical(dpi)
+    page_height = args.page_width.get_logical(dpi)
+    max_spacing = args.max_spacing.get_logical(dpi)
 
     joiner = Joiner(
             page_width = page_width, page_height = page_height,
             margin_top = 8*MM, margin_bottom = 15*MM, spacing = 8*MM,
             fill_page_height = args.fill_page_height,
-            max_spacing = args.max_spacing.get_logical(dpi = DPI),
+            max_spacing = max_spacing,
             )
 
     page_number = 1
@@ -102,7 +106,7 @@ def main():
         with Image.open(infile) as im:
             if not joiner.can_add(im):
                 page = joiner.layout_page()
-                page.save(outfile % page_number, dpi = (DPI, DPI)) # (DPI_x, DPI_y)
+                page.save(outfile % page_number, dpi = (dpi, dpi)) # (DPI_x, DPI_y)
                 page_number += 1
                 joiner.clear()
 
